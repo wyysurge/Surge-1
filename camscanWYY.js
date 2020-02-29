effc32d7afcd237923ad075953d64fe957ca3821
xiaoming123m <49673642+xiaoming123m@users.noreply.github.com>
2020-02-29 10:10:39 +0800

    Update camscanWYY.js

--- camscanWYY.js
+++ camscanWYY.js
@@ -13,5 +13,5 @@ QX & Q & Surge Mitm = ap*.intsig.net,
 */
 
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"3643731900"}}};
 $done({body: JSON.stringify(obj)});

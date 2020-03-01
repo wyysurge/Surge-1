6cd95ec68f730a37a54a01eab2d90703574cc190
xiaoming123m <49673642+xiaoming123m@users.noreply.github.com>
2020-03-01 09:24:49 +0800

    Update llydW.js

--- llydW.js
+++ llydW.js
@@ -1,31 +1,24 @@
 
 
-^https?:\/\/vira\.llsapp\.com\/api\/v2\/readings\/(accessible|limitation) url script-response-body https://github.com/xiaoming123m/Surge/blob/master/llydW.js
+http-response ^https?:\/\/vira\.llsapp\.com\/api\/v2\/readings\/(accessible|limitation) requires-body=1,max-size=0,script-path=https://github.com/xiaoming123m/Surge/blob/master/llydW.js
 MitM=vira.llsapp.com
 
-var obj = $response.body;
-var met = $request.method;
+var body = $response.body;
 var url = $request.url;
+var obj = JSON.parse(body);
 
 const vip = '/api/v2/readings/limitation';
 const time = '/api/v2/readings/accessible';
 
-if (met == "GET"){
-    if (url.indexOf(vip) != -1) {
-    obj = {
-    "modules": [],
-    "auditionDuration": 7200
-          };
-     }
+if (url.indexOf(vip) != -1) {
+    obj ["modules"] = [];
+    obj ["auditionDuration"] = 7200;
+    body = JSON.stringify(obj);
 }
-
-if (met == "GET"){
-    if (url.indexOf(time) != -1) {
-    obj = {
-    "from": 1482071586,
-    "to": 1671373986,
-    "ids": []
-           };
-      }
+if (url.indexOf(time) != -1) {
+    obj["from"] = 1482071586;
+    obj["to"] = 1671373986;
+    body = JSON.stringify(obj);
 }
-$done({body: JSON.stringify(obj)});
+$done({body});
+    

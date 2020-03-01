c296d36102eaf45c8ef534d0b4f755a53d53b8ac
xiaoming123m <49673642+xiaoming123m@users.noreply.github.com>
2020-03-01 09:04:45 +0800

    Create llydW.js

---
+++ llydW.js
@@ -0,0 +1,32 @@
+
+
+^https?:\/\/vira\.llsapp\.com\/api\/v2\/readings\/(accessible|limitation) url script-response-body https://github.com/xiaoming123m/Surge/new/master/llydW.js
+
+MitM=vira.llsapp.com
+
+var obj = $response.body;
+var met = $request.method;
+var url = $request.url;
+
+const vip = '/api/v2/readings/limitation';
+const time = '/api/v2/readings/accessible';
+
+if (met == "GET"){
+    if (url.indexOf(vip) != -1) {
+    obj = {
+    "modules": [],
+    "auditionDuration": 7200
+          };
+     }
+}
+
+if (met == "GET"){
+    if (url.indexOf(time) != -1) {
+    obj = {
+    "from": 1482071586,
+    "to": 1671373986,
+    "ids": []
+           };
+      }
+}
+$done({body: JSON.stringify(obj)});
